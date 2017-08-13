8af3865361510164173d037da6b16dc6d14f4a4e
niksw7 <niksw7@gmail.com>
2017-08-13 20:29:12 +0530

    Added test properties for restcontroller test

--- players/src/test/java/com/loreans/restcontroller/PlayerApisControllerTest.kt
+++ players/src/test/java/com/loreans/restcontroller/PlayerApisControllerTest.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
@@ -22,6 +23,7 @@ import org.springframework.web.context.WebApplicationContext
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = arrayOf(PlayersDomainAppStarter::class))
 @ContextConfiguration(classes = arrayOf(Configurations::class))
+@TestPropertySource("/test.properties")
 @WebAppConfiguration
 class PlayerApisControllerTest {
     private lateinit var mockMvc: MockMvc
